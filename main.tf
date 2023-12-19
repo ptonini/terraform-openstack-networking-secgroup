8b0e4b495520e41db6bb16d5500375c0a2ee436e
ptonini <pedro.tonini@hotmail.com>
2023-12-18 22:48:01 -0300

    fix: refactored rules variable

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ resource "openstack_networking_secgroup_v2" "this" {
 
 resource "openstack_networking_secgroup_rule_v2" "this" {
   for_each          = var.rules
-  direction         = try(each.value["direction"], "ingress")
-  ethertype         = try(each.value["ethertype"], "IPv4")
-  protocol          = try(each.value["protocol"], "tcp")
-  port_range_min    = each.value["port_range_min"]
-  port_range_max    = try(each.value["port_range_max"], each.value["port_range_min"])
-  remote_ip_prefix  = each.value["remote_ip_prefix"]
   security_group_id = openstack_networking_secgroup_v2.this.id
+  direction         = each.value.direction
+  ethertype         = each.value.ethertype
+  protocol          = each.value.protocol
+  port_range_min    = each.value.port_range_min
+  port_range_max    = coalesce(each.value.port_range_max, each.value.port_range_min)
+  remote_ip_prefix  = each.value.remote_ip_prefix
 }
--- variables.tf
+++ variables.tf
@@ -5,5 +5,13 @@ variable "description" {
 }
 
 variable "rules" {
+  type = map(object({
+    direction        = optional(string, "ingress")
+    ethertype        = optional(string, "IPv4")
+    protocol         = optional(string, "tcp")
+    port_range_min   = string
+    port_range_max   = optional(string)
+    remote_ip_prefix = string
+  }))
   default = {}
 }
